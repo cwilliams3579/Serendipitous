500230497134ed8783a33d11175208b0cbc1d40b
cwilliams3579 <clevelandwilliams3579@gmail.com>
2017-07-12 22:09:33 -0400

    Update form with clickable star links

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,15 @@
 class ProductsController < InheritedResources::Base
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
+  def show
+    @reviews = Review.where(product_id: @product.id).order("created_at DESC")
+    if @reviews.blank?
+      @avg_rating = 0
+    else
+      @avg_rating = @reviews.average(:rating).round(2)
+    end
+  end
 
   def create
     @product = Product.new(product_params)
@@ -15,6 +24,10 @@ class ProductsController < InheritedResources::Base
   end
 
   private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
     def product_params
       params.require(:product).permit(:image, :name, :description, :price, :user_id)
