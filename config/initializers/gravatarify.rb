7321e488c55a0af09b9d380f9b6a7dac5969378d
cwilliams3579 <clevelandwilliams3579@gmail.com>
2017-07-12 23:01:53 -0400

    update gravatarify.rb

--- config/initializers/gravatarify.rb
+++ config/initializers/gravatarify.rb
@@ -1,4 +1,2 @@
-Gravatarify.styles[:mini] = { :size => 20, :html => { :class => 'gravatar gravatar-mini' } }
-Gravatarify.styles[:med] = { :size => 60, :html => { :class => 'gravatar gravatar-med' } }
-Gravatarify.styles[:lar] = { :size => 80, :html => { :class => 'gravatar gravatar-lar' } }
+Gravatarify.styles[:mini] = { :size => 16, :html => { :class => 'gravatar gravatar-mini' } }
 Gravatarify.styles[:default] = { :size => 45, :html => { :class => 'gravatar' } }
