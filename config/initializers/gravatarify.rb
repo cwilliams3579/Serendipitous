2592a12a1ec5c8e8a1dbd309c5709f5cb853187f
cwilliams3579 <clevelandwilliams3579@gmail.com>
2017-07-12 23:16:48 -0400

    update product show page

--- config/initializers/gravatarify.rb
+++ config/initializers/gravatarify.rb
@@ -1,2 +1,2 @@
-Gravatarify.styles[:mini] = { :size => 16, :html => { :class => 'gravatar gravatar-mini' } }
+Gravatarify.styles[:mini] = { :size => 60, :html => { :class => 'gravatar gravatar-mini' } }
 Gravatarify.styles[:default] = { :size => 45, :html => { :class => 'gravatar' } }
