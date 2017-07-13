467acf88602bab71e839a6a0dd0b6437e4c11628
cwilliams3579 <clevelandwilliams3579@gmail.com>
2017-07-13 00:27:05 -0400

    update article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   mount_uploader :image, ImageUploader
   validates :image, presence: true
-  validates :title, presence: true, length: { in: 3..22 }
-  validates :description, presence: true, length: { in: 5..400 }
+  validates :title, presence: true, length: { in: 3..50 }
+  validates :description, presence: true, length: { in: 10..400
   belongs_to :user
 end
